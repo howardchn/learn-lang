3ba7cf7a2d3032705f5d436e06130ddaf811d007
howardch@outlook.com <howardch@outlook.com>
2016-08-03 12:52:28 +0800

    Tried eval and literal_eval.

---
+++ python/practice/suggestions91.py
@@ -0,0 +1,19 @@
+
+# assert a == b, 'not equal'
+
+def fib():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+# from itertools import islice
+# print(list(islice(fib(), 5)))
+
+from ast import literal_eval
+a = literal_eval('5 + 6')
+print(a)
+
+a, b = 5, 6
+literal_eval('a, b = b, a')
+print(b)
