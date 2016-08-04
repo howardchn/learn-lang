e3b5ca398da230ebd49f09295a1aa07f8067ed8b
howardch@outlook.com <howardch@outlook.com>
2016-08-04 11:23:00 +0800

    Fixed namespaces issue.

--- python/utils-io/remap_output.py
+++ python/utils-io/remap_output.py
@@ -1,14 +1,15 @@
 from xml.etree import ElementTree
 from os import path
 from io import open
+import os
 
-def load_csproj(csproj_path):
+def remap_output(csproj_path):
     # ElementTree.register_namespace('xmlns', 'http://schemas.microsoft.com/developer/msbuild/2003')
     tree = ElementTree.parse(csproj_path)
     root = tree.getroot()
     namespace = {'ns':'{http://schemas.microsoft.com/developer/msbuild/2003}'}
 
-    property_groups_els = [elements for elements in root.findall('%(ns)sPropertyGroup' % namespace) if not elements.find('%(ns)sOutputPath' % namespace) == None] 
+    property_groups_els = [elements for elements in root.findall('%(ns)sPropertyGroup'%namespace) if not elements.find('%(ns)sOutputPath' % namespace) == None] 
     for property_group_els in property_groups_els:
         isdebug = 'Debug' in property_group_els.get('Condition')
         output_path_els = property_group_els.find('%(ns)sOutputPath' % namespace)
@@ -16,7 +17,10 @@ def load_csproj(csproj_path):
         target_path = find_relpath(csproj_path, 'ThinkGeo', isdebug);
 
         output_path_els.text = target_path
+
+        ElementTree.register_namespace('', 'http://schemas.microsoft.com/developer/msbuild/2003')
         tree.write(csproj_path)
+        print('Remap', path.basename(csproj_path), 'completed.')
 
 def find_relpath(csproj_path, target_basename, debug = True):
     rel_path = ''
@@ -29,6 +33,7 @@ def find_relpath(csproj_path, target_basename, debug = True):
     return target_path
 
 source_path = r'E:\ThinkGeo\MapSuite\MapSuite\Layers\Background\Background-Windows\BackgroundForGisEditor-Windows.csproj'
-load_csproj(source_path)
-print('task complete.')
+remap_output(source_path)
+
+print('Task complete.')
     
