7d3ca4e7b18d195d86ab7451a4b28bb617106198
howardch@outlook.com <howardch@outlook.com>
2016-08-04 11:00:06 +0800

    Change change on csproj, but schema has some problem.

--- python/utils-io/elementtree.py
+++ python/utils-io/elementtree.py
@@ -1,27 +1,34 @@
 from xml.etree import ElementTree
 from os import path
-import os
+from io import open
 
-def load_csproj(source_path):
-    root = ElementTree.parse(source_path)
-    root = root.getroot()
+def load_csproj(csproj_path):
+    # ElementTree.register_namespace('xmlns', 'http://schemas.microsoft.com/developer/msbuild/2003')
+    tree = ElementTree.parse(csproj_path)
+    root = tree.getroot()
     namespace = {'ns':'{http://schemas.microsoft.com/developer/msbuild/2003}'}
 
-    property_group_outputs = root.findall('%(ns)sPropertyGroup/%(ns)sOutputPath' % namespace) 
-    for output in property_group_outputs:
-        print(output)
+    property_groups_els = [elements for elements in root.findall('%(ns)sPropertyGroup' % namespace) if not elements.find('%(ns)sOutputPath' % namespace) == None] 
+    for property_group_els in property_groups_els:
+        isdebug = 'Debug' in property_group_els.get('Condition')
+        output_path_els = property_group_els.find('%(ns)sOutputPath' % namespace)
+        source_path = output_path_els.text
+        target_path = find_relpath(csproj_path, 'ThinkGeo', isdebug);
 
-def find_relpath(source_path, target_basename, debug = True):
+        output_path_els.text = target_path
+        tree.write(csproj_path)
+
+def find_relpath(csproj_path, target_basename, debug = True):
     rel_path = ''
-    source_path = path.dirname(source_path)
+    source_path = path.dirname(csproj_path)
     while not path.basename(source_path) == target_basename:
         source_path = path.dirname(source_path)
         rel_path = rel_path + '..\\'
 
-    target_path = path.join(rel_path, 'MapSuiteGisEditor\MapSuiteGisEditor\MapSuiteGisEditor\bin', debug and 'debug' or 'release')
-    print(target_path)
+    target_path = path.join(rel_path, r'MapSuiteGisEditor\MapSuiteGisEditor\MapSuiteGisEditor\bin', debug and 'debug' or 'release')
+    return target_path
 
-source_path = r"E:\ThinkGeo\MapSuite\MapSuite\Layers\AdornmentLayers\AdornmentLayers-Windows\AdornmentLayersForGisEditor-Windows.csproj"
-# load_csproj(path)
-find_relpath(source_path, 'ThinkGeo')
+source_path = r'E:\ThinkGeo\MapSuite\MapSuite\Layers\Background\Background-Windows\BackgroundForGisEditor-Windows.csproj'
+load_csproj(source_path)
+print('task complete.')
     
