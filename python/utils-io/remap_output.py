e9068ba0943f5a921db4283648ff47217fb87dc3
howardch@outlook.com <howardch@outlook.com>
2016-08-04 13:29:26 +0800

    Added change copy local to false.

--- python/utils-io/remap_output.py
+++ python/utils-io/remap_output.py
@@ -16,12 +16,20 @@ def remap_output(csproj_path):
         output_path_els = property_group_els.find('%(ns)sOutputPath' % namespace)
         source_path = output_path_els.text
         target_path = find_relpath(csproj_path, 'ThinkGeo', isdebug);
-
         output_path_els.text = target_path
 
-        ElementTree.register_namespace('', 'http://schemas.microsoft.com/developer/msbuild/2003')
-        tree.write(csproj_path)
-        print('Remap', path.basename(csproj_path), 'completed.')
+    projectrefs_els = [elements for elements in root.findall('%(ns)sItemGroup/%(ns)sProjectReference'%namespace)]
+    for projectref_els in projectrefs_els:
+        private_element = projectref_els.find('%(ns)sPrivate'%namespace)
+        if private_element == None:
+            private_element = ElementTree.Element('%(ns)sPrivate'%namespace)
+            projectref_els.append(private_element)
+        private_element.text = str(False)
+
+    ElementTree.register_namespace('', 'http://schemas.microsoft.com/developer/msbuild/2003')
+    tree.write(csproj_path, xml_declaration=True, encoding='utf-8')
+    print('Remap', path.basename(csproj_path), 'completed.')
+    
 
 def find_relpath(csproj_path, target_basename, debug = True):
     rel_path = ''
@@ -36,8 +44,8 @@ def find_relpath(csproj_path, target_basename, debug = True):
 def need_remap(csproj_path):
     return csproj_path.endswith('ForGisEditor-Windows.csproj')
 
-# dirname = r'E:\ThinkGeo\MapSuite\MapSuite\Layers'
-dirname = r'E:\ThinkGeo\MapSuite\MapSuite\Styles'
+# dirname = r'E:\ThinkGeo\MapSuite\MapSuite'
+dirname = r'E:\ThinkGeo\MapSuite\MapSuite'
 for csproj_path in list_files(dirname, '.csproj'):
     need_remap = csproj_path.endswith('ForGisEditor-Windows.csproj')
     if need_remap: 
