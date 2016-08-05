e5870128e9222f43972e528def869e3669a57a24
Howard Chen <howardch@outlook.com>
2016-08-05 20:40:29 +0800

    Added clean comments from folder.

--- python/utils-io/cleancmt.py
+++ python/utils-io/cleancmt.py
@@ -3,12 +3,10 @@
 import io
 import os
 import sys
+from ospathex import list_files
 
-def clean(path):
-    file = None
-
-    try:
-        file = io.open(path, 'r+')
+def clean_file(path):
+    with io.open(path, 'r+') as file:
         lines = []
         for line in file:
             is_empty = line.strip('\n') == ''
@@ -25,19 +23,26 @@ def clean(path):
         file.seek(0)
         file.truncate()
         file.writelines(lines)
-    finally:
-        file.close()
 
-path = None
-if len(sys.argv) == 2:
-    path = sys.argv[1]
-else:
-    print('Input path to clean comments. using: cleancmt [path]')
-    path = input('path=')
-    if not os.path.exists(path):
-        raise Exception('File not found.')
+def clean_dir(dir):
+    for file_path in list_files(dir, '.cs'):
+        clean_file(file_path)
+
+def clean(path):
+    if os.path.isdir(path): clean_dir(path)
+    else: clean_file(path)
+
+path = r'C:\Users\Howard\Source\Repos\SlimHill\SlimHill.MapKit.Kernel\SlimHill.MapKit.Kernel.Windows\Layers\Index\RTree'
+# if len(sys.argv) == 2:
+#     path = sys.argv[1]
+# else:
+#     print('Input path to clean comments. using: cleancmt [path]')
+#     path = input('path=')
+#     if not os.path.exists(path):
+#         raise Exception('File not found.')
 
 if __name__ == "__main__":
     clean(path)
+    print('Complete.')
 
 
