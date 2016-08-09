e1624f82610dbf797dce15a798cdbc92613dd8ab
Howard Chen <howardch@outlook.com>
2016-08-09 21:50:04 +0800

    Added decorator method.

--- python/practice/chp04.py
+++ python/practice/chp04.py
@@ -30,18 +30,52 @@ aset = (i for i in range(1, 6) if i % 3 == 1)
 # for i in aset:
 #     print(i)
 
-a = {1, 2, 3}
-b = {3, 4, 5}
-c = a & b
+# a = {1, 2, 3}
+# b = {3, 4, 5}
+# c = a & b
 # print(c)
 
-def say1(*args):
-    print('My argument1 is', args)
 
-def say2(**args):
-    print('My argument2 is', args)
 
-say1(1, 3, 5)
-say2(a=1, b=2, c=3)
+# def say1(*args):
+#     print('My argument1 is', args)
+
+# def say2(**args):
+#     print('My argument2 is', args)
+
+# say1(1, 3, 5)
+# say2(a=1, b=2, c=3)
+
+
+# def print_if_true(thing, check):
+#     '''
+#     Prints the first argument if the second argument is true.
+#     '''
+#     if check: 
+#         print(thing)
+
+# def print_songs(s, func):
+#     for song in s:
+#         print(func(song))
+
+# songs = ['hello', 'hero', 'without you']
+# print_songs(songs, lambda s: ''.join([s.capitalize(), '!']))
+
+# def doc_it(func):
+#     def doc_it_core(*args, **kwargs):
+#         print(func.__name__, 'starting')
+#         print('args', args)
+#         print('kwargs', kwargs)
+#         result = func(*args)
+#         print(result)
+#         print(func.__name__, 'completed')
+#         return result
+#     return doc_it_core
+
+# @doc_it
+# def add(a, b, **c):
+#     return a + b
+
+# add(5, 3, name='Howard')
 
 
