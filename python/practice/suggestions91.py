660824407ea01d53055b509cb1c1508317fcc0a0
howardch@outlook.com <howardch@outlook.com>
2016-08-09 09:24:21 +0800

    Added string.format.

--- python/practice/suggestions91.py
+++ python/practice/suggestions91.py
@@ -10,10 +10,13 @@ def fib():
 # from itertools import islice
 # print(list(islice(fib(), 5)))
 
-from ast import literal_eval
-a = literal_eval('5 + 6')
-print(a)
+# from ast import literal_eval
+# a = literal_eval('5 + 6')
+# print(a)
 
-a, b = 5, 6
-literal_eval('a, b = b, a')
-print(b)
+# a, b = 5, 6
+# literal_eval('a, b = b, a')
+# print(b)
+
+a = 'Hello {0}'.format('Howard')
+print(a)
