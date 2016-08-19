ae70305963c467fcd05be21b3e47ccb47e068488
howardch@outlook.com <howardch@outlook.com>
2016-08-19 11:17:15 +0800

    Added promise test.

--- python/practice/suggestions91.py
+++ python/practice/suggestions91.py
@@ -18,5 +18,9 @@ def fib():
 # literal_eval('a, b = b, a')
 # print(b)
 
-a = 'Hello {0}'.format('Howard')
-print(a)
+# a = 'Hello {0}'.format('Howard')
+# print(a)
+
+a = [1, 2, 4, 6]
+b = [i ** 2 if i % 2 == 0 else i + 1 for i in a]
+print(b)
