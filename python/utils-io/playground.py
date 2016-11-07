f1111a5af84001834a88cce6d41119cb9a6f1eb5
howardch <howardch@outlook.com>
2016-11-07 20:38:24 +0800

    Added some test.

--- python/utils-io/playground.py
+++ python/utils-io/playground.py
@@ -1,12 +1,26 @@
+#coding=utf-8
+
 from io import open
 from utils.ospathex import list_files
 
-files = list_files(r'E:\ThinkGeo\MapSuite\MapSuite', '.projitems')
-for file_path in files:
-    with open(file_path, 'r') as file:
-        for line in file:
-            if 'Resources.Designer.cs' in line:
-                print(file_path)
-                break
+# files = list_files(r'E:\ThinkGeo\MapSuite\MapSuite', '.projitems')
+# for file_path in files:
+#     with open(file_path, 'r') as file:
+#         for line in file:
+#             if 'Resources.Designer.cs' in line:
+#                 print(file_path)
+#                 break
 
-print('done')
+file_path = r"C:\Users\howar\Desktop\temp.txt"
+with open(file_path, 'r+') as file:
+    newLines = []
+    for line in file:
+        if line.strip():
+            if not '粤语版' in line:
+                newLines.append(line)
+    file.seek(0)
+    file.truncate()
+    file.writelines(newLines)
+
+print('done.')
+
